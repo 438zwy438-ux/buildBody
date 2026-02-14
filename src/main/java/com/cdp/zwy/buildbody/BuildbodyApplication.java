70da5bc5463dec2af8fc5525a22f7e67bcf9eb00
zwy666 <3429317309@qq.com>
2026-02-14 16:56:42 +0800

    初始化

---
+++ src/main/java/com/cdp/zwy/buildbody/BuildbodyApplication.java
@@ -0,0 +1,13 @@
+package com.cdp.zwy.buildbody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildbodyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildbodyApplication.class, args);
+    }
+
+}
